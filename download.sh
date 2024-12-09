85b57fb9ceafbe4d78b632a68ee5314a13d4559a
Pavanakumar Mohanamuraly <mpkumar@cerfacs.fr>
2024-12-09 09:54:01 +0100

    Added download script

---
+++ download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.38.5/CPM.cmake
+wget https://github.com/CLIUtils/CLI11/archive/refs/tags/v2.0.0.tar.gz \
+  -O CLI11-v2.4.2.tar.gz
+wget https://gitlab.inria.fr/scotch/scotch/-/archive/v7.0.4/scotch-v7.0.4.tar.gz
+wget https://github.com/sandialabs/Zoltan/archive/refs/tags/v3.901.tar.gz \
+  -O Zoltan-v3.901.tar.gz
+wget https://www.hdfgroup.org/package/hdf5-1-10-8-tar-gz/hdf5-1.10.8.tar.gz
+
